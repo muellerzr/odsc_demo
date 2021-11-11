6b29dd73c35252291a7a1968e7669b4c192ef2d2
Zach Mueller <muellerzr@gmail.com>
2021-11-10 20:40:55 -0600

    Init repo

---
+++ odsc_demo/arithmatic.py
@@ -0,0 +1,15 @@
+def addition(a:(int,float),b:(int,float)) -> (int,float):
+    "Adds two numbers together"
+    return a+b
+
+def subtraction(a:(int,float),b:(int,float)) -> (int,float):
+    "Subtracts b from a"
+    return b-a
+
+def multiplication(a:(int,float), b:(int,float)) -> (int,float):
+    "Multiples `a` and `b`"
+    return a*b
+
+def division(a:(int,float),b:(int,float)) -> (int,float):
+    "Divides a by b"
+    return a / b
